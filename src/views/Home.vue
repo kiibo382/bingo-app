efd4b96a6e975c27b4f9e695c921a99524a90d95
shota_kizawa <shomok0106k@gmail.com>
2021-09-11 19:15:48 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Bingo App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Bingo from '@/components/Bingo.vue' // @ is an alias to /src
+
+@Options({
+  components: {
+    Bingo
+  }
+})
+export default class Home extends Vue {}
+</script>
